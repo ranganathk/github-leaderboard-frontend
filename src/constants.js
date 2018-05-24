7560e2eade15f51bfb8f314bc65d6fa464941a94
sumanth <sumanth@codeastra.com>
2018-05-24 14:47:37 +0530

    Show success/failure after adding the token

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 export const LEADER_TYPE_ADDITIONS = 'additions';
 export const LEADER_TYPE_DELETIONS = 'deletions';
 export const LEADER_TYPE_COMMITS = 'commits';
-export const LEADER_TYPE_RECENTLY_ADDED = 'recentlyAdded';
+export const LEADER_TYPE_RECENTLY_ADDED = 'latest';
 export const ORGANIZATION_TOTAL_ADDITIONS_COLOR = 'success';
 export const ORGANIZATION_TOTAL_DELETIONS_COLOR = 'danger';
 export const ORGANIZATION_TOTAL_COMMITS_COLOR = 'primary';
+
+// bc577ba1e51f1bcdc60a2fa5d661fba445fe3111
